871054efad39f4f1dbd9612998816bb56695649f
bhuvanakannan <bhuvibhuvana158@gmail.com>
2022-12-17 15:12:06 +0530

    commit messege

--- index.js
+++ index.js
@@ -2,46 +2,46 @@
 
 x2=()=>{
     let num=document.getElementById('number').value;
-   document.getElementById('demo').innerHTML=num*2;
+   document.getElementById('demo').innerHTML=num*num;
    document.getElementById('number').value="";
 }
 x3=()=>{
     let num=document.getElementById('number').value;
-   document.getElementById('demo').innerHTML=num*3
+   document.getElementById('demo').innerHTML=num*num*num;
    document.getElementById('number').value="";
 }
 x4=()=>{
     let num=document.getElementById('number').value;
-   document.getElementById('demo').innerHTML=num*4;
+   document.getElementById('demo').innerHTML=num*num*num*num;
    document.getElementById('number').value="";
 }
 x5=()=>{
     let num=document.getElementById('number').value;
-   document.getElementById('demo').innerHTML=num*5;
+   document.getElementById('demo').innerHTML=num*num*num*num*num;
    document.getElementById('number').value="";
 }
 x6=()=>{
     let num=document.getElementById('number').value;
-   document.getElementById('demo').innerHTML=num*6;
+   document.getElementById('demo').innerHTML=num*num*num*num*num*num;
    document.getElementById('number').value="";
 }
 x7=()=>{
     let num=document.getElementById('number').value;
-   document.getElementById('demo').innerHTML=num*7;
+   document.getElementById('demo').innerHTML=num*num*num*num*num*num*num;
    document.getElementById('number').value="";
 }
 x8=()=>{
     let num=document.getElementById('number').value;
-   document.getElementById('demo').innerHTML=num*8;
+   document.getElementById('demo').innerHTML=num*num*num*num*num*num*num*num;
    document.getElementById('number').value="";
 }
 x9=()=>{
     let num=document.getElementById('number').value;
-   document.getElementById('demo').innerHTML=num*9;
+   document.getElementById('demo').innerHTML=num*num*num*num*num*num*num*num*num;
    document.getElementById('number').value="";
 }
 x10=()=>{
     let num=document.getElementById('number').value;
-   document.getElementById('demo').innerHTML=num*10;
+   document.getElementById('demo').innerHTML=num*num*num*num*num*num*num*num*num*num;
    document.getElementById('number').value="";
 }
